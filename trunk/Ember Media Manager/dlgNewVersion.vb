c22af0c46032bd38b15a25518321cac6bed7ef11
jason.schnitzler@gmail.com <jason.schnitzler@gmail.com@b67298d4-4026-11de-998b-8fedbbe774c7>
2009-06-09 19:03:02 +0000

    Fix for getting IMDB from any non-conforming nfo when no usable data files are present Forgot to commit dlgNewVersion with the last commit

---
+++ trunk/Ember Media Manager/dlgNewVersion.vb
@@ -0,0 +1,40 @@
+﻿' ################################################################################
+' #                             EMBER MEDIA MANAGER                              #
+' ################################################################################
+' ################################################################################
+' # This file is part of Ember Media Manager.                                    #
+' #                                                                              #
+' # Ember Media Manager is free software: you can redistribute it and/or modify  #
+' # it under the terms of the GNU General Public License as published by         #
+' # the Free Software Foundation, either version 3 of the License, or            #
+' # (at your option) any later version.                                          #
+' #                                                                              #
+' # Ember Media Manager is distributed in the hope that it will be useful,       #
+' # but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+' # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+' # GNU General Public License for more details.                                 #
+' #                                                                              #
+' # You should have received a copy of the GNU General Public License            #
+' # along with Ember Media Manager.  If not, see <http://www.gnu.org/licenses/>. #
+' ################################################################################
+
+Option Explicit On
+
+Public Class dlgNewVersion
+
+    Private Sub llClick_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles llClick.LinkClicked
+        Process.Start("http://code.google.com/p/embermediamanager/downloads/list")
+    End Sub
+
+    Public Overloads Function ShowDialog(ByVal iNew As Integer) As Windows.Forms.DialogResult
+
+        '//
+        ' Overload to pass data
+        '\\
+
+        Me.lblNew.Text = String.Format(Me.lblNew.Text, iNew)
+        Me.txtChangelog.Text = Master.GetChangelog
+
+        Return MyBase.ShowDialog()
+    End Function
+End Class
