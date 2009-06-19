30a04560fa0e3ecb44df389c3684ecb4f0273018
nfnovais@gmail.com <nfnovais@gmail.com@b67298d4-4026-11de-998b-8fedbbe774c7>
2009-06-19 11:50:40 +0000

    Teaks on Multi Instance Validation

--- Ember Media Manager/frmSplash.vb
+++ Ember Media Manager/frmSplash.vb
@@ -1,5 +1,15 @@
 ﻿Public NotInheritable Class frmSplash
+    Delegate Sub DelegateToCloseForm()
 
+    ' if the welcome form is closed by the main form, it is cross-thread
+    ' operation. so we need to use the Invoke method to deal with it.
+    Public Sub CloseForm()
+        If (Me.InvokeRequired) Then
+            Me.Invoke(New DelegateToCloseForm(AddressOf CloseForm))
+        Else
+            Me.Close()
+        End If
+    End Sub
     Private Sub frmSplash_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Version.Text = String.Format("Version r{0}", My.Application.Info.Version.Revision)
 
