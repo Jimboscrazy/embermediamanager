a91ef81947fd7202eb8457f5678228c423dd00e6
nfnovais@gmail.com <nfnovais@gmail.com@b67298d4-4026-11de-998b-8fedbbe774c7>
2009-06-21 14:31:52 +0000

    Disclaimer on splash Form Small tweaks on Offline MM

--- trunk/Ember Media Manager/frmSplash.vb
+++ trunk/Ember Media Manager/frmSplash.vb
@@ -1,7 +1,26 @@
-﻿Public NotInheritable Class frmSplash
+﻿' ################################################################################
+' #                             EMBER MEDIA MANAGER                              #
+' ################################################################################
+' ################################################################################
+' # This file is part of Ember Media Manager.                                    #
+' #                                                                              #
+' # Ember Media Manager is free software: you can redistribute it and/or modify  #
+' # it under the terms of the GNU General Public License as published by         #
+' # the Free Software Foundation, either version 3 of the License, or            #
+' # (at your option) any later version.                                          #
+' #                                                                              #
+' # Ember Media Manager is distributed in the hope that it will be useful,       #
+' # but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+' # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+' # GNU General Public License for more details.                                 #
+' #                                                                              #
+' # You should have received a copy of the GNU General Public License            #
+' # along with Ember Media Manager.  If not, see <http://www.gnu.org/licenses/>. #
+
+Public NotInheritable Class frmSplash
     Delegate Sub DelegateToCloseForm()
 
-    ' if the welcome form is closed by the main form, it is cross-thread
+    ' if the splash form, is closed by the main form, it is cross-thread
     ' operation. so we need to use the Invoke method to deal with it.
     Public Sub CloseForm()
         If (Me.InvokeRequired) Then
