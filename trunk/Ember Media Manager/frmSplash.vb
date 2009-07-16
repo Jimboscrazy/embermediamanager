4b774de35aacf75fc18546af4cb1827ab6a3386d
jason.schnitzler@gmail.com <jason.schnitzler@gmail.com@b67298d4-4026-11de-998b-8fedbbe774c7>
2009-07-16 05:22:58 +0000

    Move .net check to applicationevents

--- trunk/Ember Media Manager/frmSplash.vb
+++ trunk/Ember Media Manager/frmSplash.vb
@@ -31,11 +31,6 @@ Public NotInheritable Class frmSplash
     End Sub
     Private Sub frmSplash_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Version.Text = String.Format("Version r{0}", My.Application.Info.Version.Revision)
-
-        If Not Master.GetNETVersion Then
-            MsgBox(String.Concat("Ember Media Manager requires .NET Framework version 3.5 or higher.", vbNewLine, vbNewLine, _
-                       "Please install .NET Framework version 3.5 or higher before attempting to use Ember."), MsgBoxStyle.Critical, "Unsupported .NET Version")
-        End If
     End Sub
 
 End Class
