0a16cbf82bd8cd0173854ce7fa9e12853b53ab46
jason.schnitzler@gmail.com <jason.schnitzler@gmail.com@b67298d4-4026-11de-998b-8fedbbe774c7>
2009-12-23 10:05:52 +0000

    Ignore "working title" entries for forcing title language

--- Ember Media Manager/clsScrapeIMDB.vb
+++ Ember Media Manager/clsScrapeIMDB.vb
@@ -356,7 +356,7 @@ mResult:
 
                     If rTitles.Count > 0 Then
                         For i As Integer = 1 To rTitles.Count - 1 Step 2
-                            If rTitles(i).Value.ToString.Contains(Master.eSettings.ForceTitle) Then
+                            If rTitles(i).Value.ToString.Contains(Master.eSettings.ForceTitle) AndAlso Not rTitles(i).Value.ToString.Contains(String.Concat(Master.eSettings.ForceTitle, " (working title)")) Then
                                 fTitle = rTitles(i - 1).Groups("title").Value.ToString
                                 Exit For
                             End If
