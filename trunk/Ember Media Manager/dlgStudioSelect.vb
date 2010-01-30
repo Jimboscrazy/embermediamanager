fd35113118e32d4f7be849ae12854e84b7b48b77
jason.schnitzler@gmail.com <jason.schnitzler@gmail.com@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-01-30 04:34:00 +0000

    Minor code cleanup/pet peeves

--- trunk/Ember Media Manager/dlgStudioSelect.vb
+++ trunk/Ember Media Manager/dlgStudioSelect.vb
@@ -18,8 +18,6 @@
 ' # along with Ember Media Manager.  If not, see <http://www.gnu.org/licenses/>. #
 ' ################################################################################
 
-
-
 Public Class dlgStudioSelect
 
     Private _imdbid As String = String.Empty
--- trunk/Ember Media Manager/dlgTVSource.vb
+++ trunk/Ember Media Manager/dlgTVSource.vb
@@ -18,8 +18,6 @@
 ' # along with Ember Media Manager.  If not, see <http://www.gnu.org/licenses/>. #
 ' ################################################################################
 
-
-Imports System
 Imports System.IO
 Imports System.Text.RegularExpressions
 
