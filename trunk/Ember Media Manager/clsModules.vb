322a3d708fd34e0449d6505182492037c07361c2
nfnovais@gmail.com <nfnovais@gmail.com@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-02-08 16:58:15 +0000

    First implementation of EmberModules and EmberAPI Note to jason: vbproj not committed we have different paths.. please update your and commit

---
+++ trunk/Ember Media Manager/clsModules.vb
@@ -0,0 +1,88 @@
+﻿
+Option Strict Off
+Imports System
+Imports System.IO
+
+Public Interface EmberExternalModule
+    Sub Enable()
+    Sub Disable()
+    Sub Setup()
+    Sub Init(ByRef emm As Object)
+    ReadOnly Property ModuleName() As String
+    ReadOnly Property ModuleVersion() As String
+End Interface
+
+Public Class EmberModules
+    Class _externalProcessorModuleClass
+        Public ProcessorModule As Object
+        Public Enabled As Boolean
+        Public AssemblyName As String
+    End Class
+    Class _EmberAPI
+        Private _MenuMediaList As System.Windows.Forms.ContextMenuStrip
+        Public EmberAPP As Object = frmMain
+        Sub New()
+            _MenuMediaList = frmMain.mnuMediaList
+        End Sub
+        Public ReadOnly Property MenuMediaList() As System.Windows.Forms.ContextMenuStrip
+            Get
+                Return _MenuMediaList
+            End Get
+        End Property
+    End Class
+
+    Public EmberAPI As New _EmberAPI
+    Public externalProcessorModules As New List(Of _externalProcessorModuleClass)
+    Private moduleLocation As String = Path.Combine(Application.StartupPath, "Modules")
+    Public Sub loadModules()
+        'Assembly to load the file
+        Dim assembly As System.Reflection.Assembly
+        'For each .dll file in the module directory
+        For Each file As String In System.IO.Directory.GetFiles(moduleLocation, "*.dll")
+            'Load the assembly
+            assembly = System.Reflection.Assembly.LoadFrom(file)
+            'Loop through each of the assemeblies type
+            For Each fileType As Type In assembly.GetTypes
+                Try
+                    'Activate the located module
+                    Dim t As Type = fileType.GetInterface("EmberExternalModule")
+                    If Not t Is Nothing Then
+                        Dim ProcessorModule As New Object
+                        ProcessorModule = Activator.CreateInstance(fileType)
+                        'Add the activated module to the arraylist
+                        Dim _externalProcessorModule As New _externalProcessorModuleClass
+                        _externalProcessorModule.ProcessorModule = ProcessorModule
+                        _externalProcessorModule.AssemblyName = Path.GetFileName(file)
+                        externalProcessorModules.Add(_externalProcessorModule)
+                        ProcessorModule.Init(EmberAPI)
+                    End If
+                Catch ex As Exception
+                End Try
+            Next
+        Next
+    End Sub
+
+    Dim WithEvents ModulesMenu As New System.Windows.Forms.ToolStripMenuItem
+    Sub New()
+        ModulesMenu.Text = "Modules Settings"
+        frmMain.EditToolStripMenuItem.DropDownItems.Add(ModulesMenu)
+        loadModules()
+    End Sub
+    Private Sub ModulesMenutem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ModulesMenu.Click
+        Dim modulesSetup As New dlgModuleSettings
+        For Each _externalProcessorModule In externalProcessorModules
+            Dim li As ListViewItem = modulesSetup.lstModules.Items.Add(_externalProcessorModule.ProcessorModule.ModuleName())
+            li.SubItems.Add("Disabled")
+            li.Tag = _externalProcessorModule.AssemblyName
+        Next
+        modulesSetup.ModulesManager = Me
+        modulesSetup.ShowDialog()
+    End Sub
+    Public Sub RunModuleSetup(ByVal ModuleAssembly As String)
+        For Each _externalProcessorModule In externalProcessorModules
+            If _externalProcessorModule.AssemblyName = ModuleAssembly Then
+                _externalProcessorModule.ProcessorModule.Setup()
+            End If
+        Next
+    End Sub
+End Class
---
+++ trunk/Ember Media Manager/dlgModuleSettings.vb
@@ -0,0 +1,47 @@
+﻿' ################################################################################
+' #                             EMBER MEDIA MANAGER                              #
+' ################################################################################
+' ################################################################################
+' # This file is part of Ember Media Manager.                                    #
+' #                                                                              #
+' # Ember Media Manager is free software: you can redistribute it and/or modify  #
+' # it under the terms of the GNU General Public License as published by         #
+' # the Free Software Foundation, either version 3 of the License, or            #
+' # (at your option) any later version.                                          #
+' #                                                                              #
+' # Ember Media Manager is distributed in the hope that it will be useful,       #
+' # but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+' # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+' # GNU General Public License for more details.                                 #
+' #                                                                              #
+' # You should have received a copy of the GNU General Public License            #
+' # along with Ember Media Manager.  If not, see <http://www.gnu.org/licenses/>. #
+' ################################################################################
+
+
+Imports System
+Imports System.IO
+
+Public Class dlgModuleSettings
+    Public ModulesManager As EmberModules
+
+    Private Sub lstModules_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lstModules.SelectedIndexChanged
+        If lstModules.SelectedItems.Count > 0 Then
+            btnSetup.Enabled = True
+            btnEnable.Enabled = True
+        Else
+            btnSetup.Enabled = False
+            btnEnable.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnSetup_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSetup.Click
+        If lstModules.SelectedItems.Count > 0 Then
+            ModulesManager.RunModuleSetup(lstModules.SelectedItems.Item(0).Tag().ToString)
+        End If
+    End Sub
+
+    Private Sub btnOK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOK.Click
+        Me.Close()
+    End Sub
+End Class
