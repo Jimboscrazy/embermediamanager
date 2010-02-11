4cde199a89ea5e732f4c076665c070d371638e26
nfnovais <nfnovais@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-02-11 01:50:05 +0000

    Prepare for remove

--- trunk/ScraperModuleSample/scraperModule.vb
+++ trunk/ScraperModuleSample/scraperModule.vb
@@ -1,8 +1,27 @@
-﻿Public Interface EmberScraperModule
+﻿' ################################################################################
+' #                             EMBER MEDIA MANAGER                              #
+' ################################################################################
+' ################################################################################
+' # This file is part of Ember Media Manager.                                    #
+' #                                                                              #
+' # Ember Media Manager is free software: you can redistribute it and/or modify  #
+' # it under the terms of the GNU General Public License as published by         #
+' # the Free Software Foundation, either version 3 of the License, or            #
+' # (at your option) any later version.                                          #
+' #                                                                              #
+' # Ember Media Manager is distributed in the hope that it will be useful,       #
+' # but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+' # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+' # GNU General Public License for more details.                                 #
+' #                                                                              #
+' # You should have received a copy of the GNU General Public License            #
+' # along with Ember Media Manager.  If not, see <http://www.gnu.org/licenses/>. #
+' ################################################################################
+
+Public Interface EmberScraperModule
     Sub Setup()
-    Function ExecFase1(ByVal MovieTitle As String, ByVal Id As String) As ArrayList
-    Function ExecFase2(ByVal MovieTitle As String, ByVal Id As String) As Media.Movie
-    Function ExecPost(ByVal Movie As Media.Movie) As Media.Movie
+    Function Scraper(ByVal MovieTitle As String, ByVal Id As String) As Media.Movie
+    Function PostScraper(ByVal Movie As Media.Movie) As Media.Movie
     ReadOnly Property ModuleName() As String
     ReadOnly Property ModuleVersion() As String
     ReadOnly Property IsScraper() As Boolean
@@ -11,7 +30,6 @@ End Interface
 
 Public Class TestEmberScraperModule
     Implements EmberScraperModule
-    Dim emmAPI As New Object
     Private Enabled As Boolean = False
     Private _Name As String = "Teste Scraper"
     Private _Version As String = "1.0"
@@ -30,14 +48,10 @@ Public Class TestEmberScraperModule
         Dim _setup As New frmSetup
         _setup.ShowDialog()
     End Sub
-    Function StartFase1(ByVal MovieTitle As String, ByVal Id As String) As ArrayList Implements EmberScraperModule.ExecFase1
+    Function Scraper(ByVal MovieTitle As String, ByVal Id As String) As Media.Movie Implements EmberScraperModule.Scraper
         Return Nothing
     End Function
-
-    Function StartFase2(ByVal MovieTitle As String, ByVal Id As String) As Media.Movie Implements EmberScraperModule.ExecFase2
-        Return Nothing
-    End Function
-    Function ExecPost(ByVal Movie As Media.Movie) As Media.Movie Implements EmberScraperModule.ExecPost
+    Function PostScraper(ByVal Movie As Media.Movie) As Media.Movie Implements EmberScraperModule.PostScraper
         Return Nothing
     End Function
     ReadOnly Property ModuleName() As String Implements EmberScraperModule.ModuleName
