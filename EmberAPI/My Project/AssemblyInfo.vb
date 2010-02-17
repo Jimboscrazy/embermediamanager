2347a77461adb7c192e74c03808c2e29a2fb4d61
nfnovais <nfnovais@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-02-17 10:52:41 +0000

    EmberCore Scraper DLL organization

--- EmberAPI/My Project/AssemblyInfo.vb
+++ EmberAPI/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.0.1350")> 
-<Assembly: AssemblyFileVersion("0.0.0.1350")> 
+<Assembly: AssemblyVersion("0.0.0.1354")> 
+<Assembly: AssemblyFileVersion("0.0.0.1354")> 
--- Modules/scraper.EmberCore/frmNativeSetup.Designer.vb
+++ Modules/scraper.EmberCore/frmNativeSetup.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class frmSetup
+Partial Class frmNativeSetup
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,7 +22,7 @@ Partial Class frmSetup
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSetup))
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmNativeSetup))
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel
         Me.OK_Button = New System.Windows.Forms.Button
         Me.Cancel_Button = New System.Windows.Forms.Button
