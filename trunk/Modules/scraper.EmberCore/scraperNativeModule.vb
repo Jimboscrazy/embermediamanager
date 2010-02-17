7d66e0562747dbccc71718aa1e8d920d72dbf28d
nfnovais <nfnovais@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-02-17 10:52:41 +0000

    EmberCore Scraper DLL organization

--- trunk/Modules/scraper.EmberCore/scraperNativeModule.vb
+++ trunk/Modules/scraper.EmberCore/scraperNativeModule.vb
@@ -1,67 +1,4 @@
-﻿' ################################################################################
-' #                             EMBER ProxyMEdia MANAGER                              #
-' ################################################################################
-' ################################################################################
-' # This file is part of Ember Media Manager.                                    #
-' #                                                                              #
-' # Ember Media Manager is free software: you can redistribute it and/or modify  #
-' # it under the terms of the GNU General Public License as published by         #
-' # the Free Software Foundation, either version 3 of the License, or            #
-' # (at your option) any later version.                                          #
-' #                                                                              #
-' # Ember Media Manager is distributed in the hope that it will be useful,       #
-' # but WITHOUT ANY WARRANTY; without even the implied warranty of               #
-' # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
-' # GNU General Public License for more details.                                 #
-' #                                                                              #
-' # You should have received a copy of the GNU General Public License            #
-' # along with Ember Media Manager.  If not, see <http://www.gnu.org/licenses/>. #
-' ################################################################################
-Imports System.IO
-Imports EmberAPI
-Public Class EmberXMLScraperModule
-    Implements EmberAPI.Interfaces.EmberScraperModule
-
-    Public ReadOnly Property IsPostScraper() As Boolean Implements EmberAPI.Interfaces.EmberScraperModule.IsPostScraper
-        Get
-            Return True
-        End Get
-    End Property
-
-    Public ReadOnly Property IsScraper() As Boolean Implements EmberAPI.Interfaces.EmberScraperModule.IsScraper
-        Get
-            Return True
-        End Get
-    End Property
-
-    Public ReadOnly Property ModuleName() As String Implements EmberAPI.Interfaces.EmberScraperModule.ModuleName
-        Get
-            Return "Ember XML Scraper"
-        End Get
-    End Property
-
-    Public ReadOnly Property ModuleVersion() As String Implements EmberAPI.Interfaces.EmberScraperModule.ModuleVersion
-        Get
-            Return FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly.Location).FilePrivatePart.ToString
-        End Get
-    End Property
-
-    Public Function PostScraper(ByRef DBMovie As EmberAPI.Structures.DBMovie, ByVal ScrapeType As EmberAPI.Enums.ScrapeType) As Boolean Implements EmberAPI.Interfaces.EmberScraperModule.PostScraper
-
-    End Function
-
-    Function Scraper(ByRef DBMovie As EmberAPI.Structures.DBMovie, ByRef Options As Structures.ScrapeOptions) As Boolean Implements EmberAPI.Interfaces.EmberScraperModule.Scraper
-
-    End Function
-
-    Public Event ScraperUpdateMediaList(ByVal col As Integer, ByVal v As Boolean) Implements EmberAPI.Interfaces.EmberScraperModule.ScraperUpdateMediaList
-
-    Public Sub Setup(ByVal tScraper As Integer) Implements EmberAPI.Interfaces.EmberScraperModule.Setup
-
-    End Sub
-End Class
-
-
+﻿
 ''' <summary>
 ''' Native Scraper
 ''' </summary>
@@ -93,7 +30,7 @@ Public Class EmberNativeScraperModule
         End Get
     End Property
     Sub Setup(ByVal tScraper As Integer) Implements EmberAPI.Interfaces.EmberScraperModule.Setup
-        Dim _setup As New frmSetup
+        Dim _setup As New frmNativeSetup
         _setup.lblVersion.Text = ModuleVersion
         _setup.TabControl1.SelectTab(tScraper)
         _setup.ShowDialog()
