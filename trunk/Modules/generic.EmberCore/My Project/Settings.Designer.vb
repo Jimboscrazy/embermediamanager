1f6052a0a208ab4f2e5b9e708c19e487c6e4ac62
nfnovais@gmail.com <nfnovais@gmail.com@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-02-19 11:35:42 +0000

    Finish up organization

--- trunk/Modules/generic.EmberCore/My Project/Settings.Designer.vb
+++ trunk/Modules/generic.EmberCore/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.generic.EmberVore.My.MySettings
+        Friend ReadOnly Property Settings() As Global.generic.EmberCore.My.MySettings
             Get
-                Return Global.generic.EmberVore.My.MySettings.Default
+                Return Global.generic.EmberCore.My.MySettings.Default
             End Get
         End Property
     End Module
