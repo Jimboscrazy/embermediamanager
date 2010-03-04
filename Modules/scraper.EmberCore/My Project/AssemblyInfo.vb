f435e10c5e517c021bcf50aa53e145de3315e6e7
jason.schnitzler@gmail.com <jason.schnitzler@gmail.com@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-03-04 00:50:45 +0000

    More small tweaks to movie scraper

--- Modules/scraper.EmberCore/My Project/AssemblyInfo.vb
+++ Modules/scraper.EmberCore/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.0.1534")> 
-<Assembly: AssemblyFileVersion("0.0.0.1534")> 
+<Assembly: AssemblyVersion("0.0.0.1535")> 
+<Assembly: AssemblyFileVersion("0.0.0.1535")> 
