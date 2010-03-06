734287c330cda3687e2e6e09a937c77c274630ba
jason.schnitzler@gmail.com <jason.schnitzler@gmail.com@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-03-06 07:48:43 +0000

    Enable "Change Show"

--- trunk/Modules/scraper.EmberCore/My Project/AssemblyInfo.vb
+++ trunk/Modules/scraper.EmberCore/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.0.1558")> 
-<Assembly: AssemblyFileVersion("0.0.0.1558")> 
+<Assembly: AssemblyVersion("0.0.0.1559")> 
+<Assembly: AssemblyFileVersion("0.0.0.1559")> 
