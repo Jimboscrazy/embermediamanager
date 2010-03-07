251602f9dd916599d2d5049d59beb0abd3698ddd
nfnovais <nfnovais@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-03-07 13:35:55 +0000

    Some fix related with modules ordering

--- Modules/scraper.EmberCore/frmInfoSettingsHolder.vb
+++ Modules/scraper.EmberCore/frmInfoSettingsHolder.vb
@@ -183,7 +183,7 @@ Public Class frmInfoSettingsHolder
 
     Private Sub btnDown_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDown.Click
         Dim order As Integer = ModulesManager.Instance.externalScrapersModules.FirstOrDefault(Function(p) p.AssemblyName = EmberNativeScraperModule._AssemblyName).ScraperOrder
-        If order < ModulesManager.Instance.externalScrapersModules.Count - 1 Then
+        If order < ModulesManager.Instance.externalScrapersModules.Where(Function(y) y.ProcessorModule.IsScraper).Count - 1 Then
             ModulesManager.Instance.externalScrapersModules.FirstOrDefault(Function(p) p.ScraperOrder = order + 1).ScraperOrder = order
             ModulesManager.Instance.externalScrapersModules.FirstOrDefault(Function(p) p.AssemblyName = EmberNativeScraperModule._AssemblyName).ScraperOrder = order + 1
             RaiseEvent SetupScraperChanged(cbEnabled.Checked, 1)
@@ -203,7 +203,7 @@ Public Class frmInfoSettingsHolder
     End Sub
     Sub orderChanged()
         Dim order As Integer = ModulesManager.Instance.externalScrapersModules.FirstOrDefault(Function(p) p.AssemblyName = EmberNativeScraperModule._AssemblyName).ScraperOrder
-        btnDown.Enabled = (order < ModulesManager.Instance.externalScrapersModules.Count - 1)
+        btnDown.Enabled = (order < ModulesManager.Instance.externalScrapersModules.Where(Function(y) y.ProcessorModule.IsScraper).Count - 1)
         btnUp.Enabled = (order > 0)
     End Sub
 End Class
