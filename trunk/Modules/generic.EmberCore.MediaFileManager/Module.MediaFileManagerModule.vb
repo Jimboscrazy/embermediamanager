7388b1f98b6be8c5269b09071f4ad546495166db
nfnovais <nfnovais@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-03-07 19:00:09 +0000

    more tweaks

--- trunk/Modules/generic.EmberCore.MediaFileManager/Module.MediaFileManagerModule.vb
+++ trunk/Modules/generic.EmberCore.MediaFileManager/Module.MediaFileManagerModule.vb
@@ -30,7 +30,7 @@ Imports System.Xml.Serialization
 Public Class FileManagerExternalModule
     Implements Interfaces.EmberExternalModule
     Private _enabled As Boolean = False
-    Private _Name As String = "Media File Manager"
+    Private _Name As String = Master.eLang.GetString(999, "Media File Manager")
 
     Private MyMenuSep As New System.Windows.Forms.ToolStripSeparator
     Private MyMenu As New System.Windows.Forms.ToolStripMenuItem
@@ -118,11 +118,11 @@ Public Class FileManagerExternalModule
     End Sub
 
     Sub Enable()
-        MyMenu.Text = "Media File Manager"
+        MyMenu.Text = Master.eLang.GetString(999, "Media File Manager")
 
-        MySubMenu1.Text = "Move To"
+        MySubMenu1.Text = Master.eLang.GetString(999, "Move To")
         MySubMenu1.Tag = "MOVE"
-        MySubMenu2.Text = "Copy To"
+        MySubMenu2.Text = Master.eLang.GetString(999, "Copy To")
         MySubMenu2.Tag = "COPY"
         MyMenu.DropDownItems.Add(MySubMenu1)
         MyMenu.DropDownItems.Add(MySubMenu2)
@@ -186,14 +186,14 @@ Public Class FileManagerExternalModule
                     If ItemsToWork.Count = 1 AndAlso Directory.Exists(ItemsToWork(0).ToString) Then
                         Select Case tMItem.OwnerItem.Tag.ToString
                             Case "MOVE"
-                                If MsgBox("Move from " + ItemsToWork(0).ToString + " To " + Path.Combine(tMItem.Tag.ToString, Path.GetFileName(ItemsToWork(0).ToString)), MsgBoxStyle.YesNo, "Move") = MsgBoxResult.Yes Then
+                                If MsgBox(String.Format(Master.eLang.GetString(999, "Move from {0} To {1}"), ItemsToWork(0).ToString, Path.Combine(tMItem.Tag.ToString, Path.GetFileName(ItemsToWork(0).ToString))), MsgBoxStyle.YesNo, "Move") = MsgBoxResult.Yes Then
                                     'TODO:  need to test it better
                                     DirectoryMove(ItemsToWork(0).ToString, Path.Combine(tMItem.Tag.ToString, Path.GetFileName(ItemsToWork(0).ToString)), Master.eLang.GetString(756, "Moving Movie"))
                                     Master.DB.DeleteFromDB(MovieId)
                                 End If
 
                             Case "COPY"
-                                If MsgBox("Copy from " + ItemsToWork(0).ToString + " To " + Path.Combine(tMItem.Tag.ToString, Path.GetFileName(ItemsToWork(0).ToString)), MsgBoxStyle.YesNo, "Move") = MsgBoxResult.Yes Then
+                                If MsgBox(String.Format(Master.eLang.GetString(999, "Copy from {0} To {1}"), ItemsToWork(0).ToString, Path.Combine(tMItem.Tag.ToString, Path.GetFileName(ItemsToWork(0).ToString))), MsgBoxStyle.YesNo, "Copy") = MsgBoxResult.Yes Then
                                     'TODO:   need to test it better 
                                     DirectoryCopy(ItemsToWork(0).ToString, Path.Combine(tMItem.Tag.ToString, Path.GetFileName(ItemsToWork(0).ToString)), Master.eLang.GetString(756, "Copying Movie"))
                                 End If
