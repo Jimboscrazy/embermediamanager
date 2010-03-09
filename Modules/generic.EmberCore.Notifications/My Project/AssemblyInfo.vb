f1c18fde34c8ba645ef131b2c2c7e80ebb88a438
jason.schnitzler@gmail.com <jason.schnitzler@gmail.com@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-03-09 10:00:35 +0000

    Fixes to loading of show item info

--- Modules/generic.EmberCore.Notifications/My Project/AssemblyInfo.vb
+++ Modules/generic.EmberCore.Notifications/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.0.1603")> 
-<Assembly: AssemblyFileVersion("0.0.0.1603")> 
+<Assembly: AssemblyVersion("0.0.0.1604")> 
+<Assembly: AssemblyFileVersion("0.0.0.1604")> 
 
