534a34499836f5e13a7e4e2c7f7bc1a4f1a8247d
nfnovais@gmail.com <nfnovais@gmail.com@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-03-11 15:20:35 +0000

    More Tweaks on RefreshMovie ; Move more DB Patch Code to Main Application

--- trunk/Ember Media Manager/frmMain.vb
+++ trunk/Ember Media Manager/frmMain.vb
@@ -6761,39 +6761,70 @@ doCancel:
                         tmpMovieDb.ListTitle = tTitle
                     End If
                 End If
-                Me.Invoke(myDelegate, New Object() {dRow(0), 3, tmpMovieDb.ListTitle})
-                Me.Invoke(myDelegate, New Object() {dRow(0), 15, tmpMovieDb.Movie.Title})
-                Me.Invoke(myDelegate, New Object() {dRow(0), 50, tmpMovieDb.Movie.SortTitle})
-                'update genre
-                Me.Invoke(myDelegate, New Object() {dRow(0), 26, tmpMovieDb.Movie.Genre})
-
-
-
+                If Me.InvokeRequired Then
+                    Me.Invoke(myDelegate, New Object() {dRow(0), 3, tmpMovieDb.ListTitle})
+                    Me.Invoke(myDelegate, New Object() {dRow(0), 15, tmpMovieDb.Movie.Title})
+                    Me.Invoke(myDelegate, New Object() {dRow(0), 50, tmpMovieDb.Movie.SortTitle})
+                    Me.Invoke(myDelegate, New Object() {dRow(0), 26, tmpMovieDb.Movie.Genre})
+                Else
+                    DirectCast(dRow(0), DataRow).Item(3) = tmpMovieDb.ListTitle
+                    DirectCast(dRow(0), DataRow).Item(15) = tmpMovieDb.Movie.Title
+                    DirectCast(dRow(0), DataRow).Item(50) = tmpMovieDb.Movie.SortTitle
+                    DirectCast(dRow(0), DataRow).Item(24) = tmpMovieDb.Movie.Genre
+                End If
 
                 tmpMovieDb.FileSource = APIXML.GetFileSource(tmpMovieDb.Filename)
                 Dim mContainer As New Scanner.MovieContainer With {.Filename = tmpMovieDb.Filename, .isSingle = tmpMovieDb.isSingle}
                 fScanner.GetMovieFolderContents(mContainer)
                 tmpMovieDb.PosterPath = mContainer.Poster
 
-                Me.Invoke(myDelegate, New Object() {dRow(0), 4, If(String.IsNullOrEmpty(mContainer.Poster), False, True)})
+                If Me.InvokeRequired Then
+                    Me.Invoke(myDelegate, New Object() {dRow(0), 4, If(String.IsNullOrEmpty(mContainer.Poster), False, True)})
+                Else
+                    DirectCast(dRow(0), DataRow).Item(4) = If(String.IsNullOrEmpty(mContainer.Poster), False, True)
+                End If
                 tmpMovieDb.FanartPath = mContainer.Fanart
 
-                Me.Invoke(myDelegate, New Object() {dRow(0), 5, If(String.IsNullOrEmpty(mContainer.Fanart), False, True)})
+                If Me.InvokeRequired Then
+                    Me.Invoke(myDelegate, New Object() {dRow(0), 5, If(String.IsNullOrEmpty(mContainer.Fanart), False, True)})
+                Else
+                    DirectCast(dRow(0), DataRow).Item(5) = If(String.IsNullOrEmpty(mContainer.Fanart), False, True)
+                End If
                 'assume invalid nfo if no title
                 tmpMovieDb.NfoPath = If(String.IsNullOrEmpty(tmpMovieDb.Movie.Title), String.Empty, mContainer.Nfo)
 
-                Me.Invoke(myDelegate, New Object() {dRow(0), 6, If(String.IsNullOrEmpty(tmpMovieDb.NfoPath), False, True)})
+                If Me.InvokeRequired Then
+                    Me.Invoke(myDelegate, New Object() {dRow(0), 6, If(String.IsNullOrEmpty(tmpMovieDb.NfoPath), False, True)})
+                Else
+                    DirectCast(dRow(0), DataRow).Item(6) = If(String.IsNullOrEmpty(tmpMovieDb.NfoPath), False, True)
+                End If
                 tmpMovieDb.TrailerPath = mContainer.Trailer
 
-                Me.Invoke(myDelegate, New Object() {dRow(0), 7, If(String.IsNullOrEmpty(mContainer.Trailer), False, True)})
+                If Me.InvokeRequired Then
+                    Me.Invoke(myDelegate, New Object() {dRow(0), 7, If(String.IsNullOrEmpty(mContainer.Trailer), False, True)})
+                Else
+                    DirectCast(dRow(0), DataRow).Item(7) = If(String.IsNullOrEmpty(mContainer.Trailer), False, True)
+                End If
                 tmpMovieDb.SubPath = mContainer.Subs
 
-                Me.Invoke(myDelegate, New Object() {dRow(0), 8, If(String.IsNullOrEmpty(mContainer.Subs), False, True)})
+                If Me.InvokeRequired Then
+                    Me.Invoke(myDelegate, New Object() {dRow(0), 8, If(String.IsNullOrEmpty(mContainer.Subs), False, True)})
+                Else
+                    DirectCast(dRow(0), DataRow).Item(8) = If(String.IsNullOrEmpty(mContainer.Subs), False, True)
+                End If
                 tmpMovieDb.ExtraPath = mContainer.Extra
 
-                Me.Invoke(myDelegate, New Object() {dRow(0), 9, If(String.IsNullOrEmpty(mContainer.Extra), False, True)})
+                If Me.InvokeRequired Then
+                    Me.Invoke(myDelegate, New Object() {dRow(0), 9, If(String.IsNullOrEmpty(mContainer.Extra), False, True)})
+                Else
+                    DirectCast(dRow(0), DataRow).Item(9) = If(String.IsNullOrEmpty(mContainer.Extra), False, True)
+                End If
 
-                Me.Invoke(myDelegate, New Object() {dRow(0), 1, tmpMovieDb.Filename})
+                If Me.InvokeRequired Then
+                    Me.Invoke(myDelegate, New Object() {dRow(0), 1, tmpMovieDb.Filename})
+                Else
+                    DirectCast(dRow(0), DataRow).Item(1) = tmpMovieDb.Filename
+                End If
                 tmpMovieDb.IsMark = Convert.ToBoolean(DirectCast(dRow(0), DataRow).Item(11))
                 tmpMovieDb.IsLock = Convert.ToBoolean(DirectCast(dRow(0), DataRow).Item(14))
 
