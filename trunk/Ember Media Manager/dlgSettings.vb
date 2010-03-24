f157dca6490c20ffb3c382e2dea2f1c1b137045d
nfnovais <nfnovais@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-03-24 21:53:08 +0000

    Tweaks ; Support to Beta Releases

--- trunk/Ember Media Manager/dlgSettings.vb
+++ trunk/Ember Media Manager/dlgSettings.vb
@@ -4391,17 +4391,13 @@ Public Class dlgSettings
 
     Class ListViewItemComparer
         Implements IComparer
-
         Private col As Integer
-
         Public Sub New()
             col = 0
         End Sub
-
         Public Sub New(ByVal column As Integer)
             col = column
         End Sub
-
         Public Function Compare(ByVal x As Object, ByVal y As Object) As Integer _
            Implements IComparer.Compare
             Return [String].Compare(CType(x, ListViewItem).SubItems(col).Text, CType(y, ListViewItem).SubItems(col).Text)
