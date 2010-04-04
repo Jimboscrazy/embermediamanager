2147cd67ba88cae752b8437120c9f293bd79e87d
nfnovais <nfnovais@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-04-04 09:53:34 +0000

    Change Ember UAC privilegeLevel ; XMLscraper Manual GetTrailer

---
+++ trunk/Modules/scraper.EmberCore.XML/dlgPleaseWait.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Windows.Forms
+
+Public Class dlgPleaseWait
+
+    Private Sub Cancel_Button_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+    End Sub
+
+    Private Sub dlgPleaseWait_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        SetUp()
+    End Sub
+    Sub SetUp()
+        Me.Text = Master.eLang.GetString(9, "Loading Please Wait")
+        Me.Label1.Text = Master.eLang.GetString(6, "Downloading details...")
+        Me.Cancel_Button.Text = Master.eLang.GetString(167, "Cancel", True)
+    End Sub
+End Class
