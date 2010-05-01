b7a3670bac6f9f882cf6cbd27eebbd3f705d8a30
nfnovais <nfnovais@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-05-01 02:07:51 +0000

    Fix Bug #547 Largest poster sizes are not always available even if scraped properly

--- addons/scraper.EmberCore/Scraper/dlgImgSelect.vb
+++ addons/scraper.EmberCore/Scraper/dlgImgSelect.vb
@@ -1087,7 +1087,7 @@ Public Class dlgImgSelect
 
     Private Sub SetupSizes(ByVal sURL As String)
         Try
-            Dim sLeft As String = Strings.Left(sURL, sURL.Length - 10)
+            Dim sLeft As String = Strings.Left(sURL, sURL.Length - 10).Substring(sURL.IndexOf("/", 7))
 
             Me.rbXLarge.Checked = False
             Me.rbXLarge.Enabled = False
@@ -1105,28 +1105,28 @@ Public Class dlgImgSelect
 
             For i As Integer = 0 To Me.TMDBPosters.Count - 1
                 Select Case True
-                    Case Me.TMDBPosters.Item(i).URL = String.Concat(sLeft, "-original.jpg")
+                    Case Me.TMDBPosters.Item(i).URL.Substring(sURL.IndexOf("/", 7)) = String.Concat(sLeft, "-original.jpg")
                         ' xlarge
                         If Not Master.eSettings.UseImgCache OrElse Not IsNothing(TMDBPosters.Item(i).WebImage.Image) Then
                             Me.rbXLarge.Enabled = True
                             Me.rbXLarge.Tag = Me.TMDBPosters.Item(i).URL
                             If Master.eSettings.UseImgCache Then Me.rbXLarge.Text = String.Format(Master.eLang.GetString(51, "Original ({0}x{1})"), Me.TMDBPosters.Item(i).WebImage.Image.Width, Me.TMDBPosters.Item(i).WebImage.Image.Height)
                         End If
-                    Case Me.TMDBPosters.Item(i).URL = String.Concat(sLeft, "-cover.jpg")
+                    Case Me.TMDBPosters.Item(i).URL.Substring(sURL.IndexOf("/", 7)) = String.Concat(sLeft, "-cover.jpg")
                         ' large
                         If Not Master.eSettings.UseImgCache OrElse Not IsNothing(TMDBPosters.Item(i).WebImage.Image) Then
                             Me.rbLarge.Enabled = True
                             Me.rbLarge.Tag = Me.TMDBPosters.Item(i).URL
                             If Master.eSettings.UseImgCache Then Me.rbLarge.Text = String.Format(Master.eLang.GetString(52, "Cover ({0}x{1})"), Me.TMDBPosters.Item(i).WebImage.Image.Width, Me.TMDBPosters.Item(i).WebImage.Image.Height)
                         End If
-                    Case Me.TMDBPosters.Item(i).URL = String.Concat(sLeft, "-thumb.jpg")
+                    Case Me.TMDBPosters.Item(i).URL.Substring(sURL.IndexOf("/", 7)) = String.Concat(sLeft, "-thumb.jpg")
                         ' small
                         If Not Master.eSettings.UseImgCache OrElse Not IsNothing(TMDBPosters.Item(i).WebImage.Image) Then
                             Me.rbSmall.Enabled = True
                             Me.rbSmall.Tag = Me.TMDBPosters.Item(i).URL
                             If Master.eSettings.UseImgCache Then Me.rbSmall.Text = String.Format(Master.eLang.GetString(53, "Small ({0}x{1})"), Me.TMDBPosters.Item(i).WebImage.Image.Width, Me.TMDBPosters.Item(i).WebImage.Image.Height)
                         End If
-                    Case Me.TMDBPosters.Item(i).URL = sURL '-mid.jpg
+                    Case Me.TMDBPosters.Item(i).URL.Substring(sURL.IndexOf("/", 7)) = sURL.Substring(sURL.LastIndexOf("/")) '-mid.jpg
                         If Master.eSettings.UseImgCache Then Me.rbMedium.Text = String.Format(Master.eLang.GetString(54, "Medium ({0}x{1})"), Me.TMDBPosters.Item(i).WebImage.Image.Width, Me.TMDBPosters.Item(i).WebImage.Image.Height)
                 End Select
             Next
