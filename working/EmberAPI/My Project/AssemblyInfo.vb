d513f33d98abdbc2faeaa43471b5bd86634c6c05
nfnovais@gmail.com <nfnovais@gmail.com@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-05-10 08:43:22 +0000

    New working branch

---
+++ working/EmberAPI/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("EmberAPI")> 
+<Assembly: AssemblyDescription("www.embermm.com")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("EmberAPI")> 
+<Assembly: AssemblyCopyright("Copyright © 2009-2010")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8b4c3757-1e73-4f81-a5d2-78490dd1ae01")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("0.0.0.2215")> 
+<Assembly: AssemblyFileVersion("0.0.0.2215")> 
