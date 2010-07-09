45cc90fd5048ed810edca9204aa5e5c822408836
nfnovais <nfnovais@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-07-09 22:49:25 +0000

    Wip on InOut Modules (final SVN cleanup)

--- addons/inout.Matroska/My Project/Settings.Designer.vb
+++ addons/inout.Matroska/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.inout.Matroska.My.MySettings
+        Friend ReadOnly Property Settings() As Global.InOut.Matroska.My.MySettings
             Get
-                Return Global.inout.Matroska.My.MySettings.Default
+                Return Global.InOut.Matroska.My.MySettings.Default
             End Get
         End Property
     End Module
