ee4ca39e3555f42dcdcfb4495493d7f93c1dfb7e
nfnovais@gmail.com <nfnovais@gmail.com@b67298d4-4026-11de-998b-8fedbbe774c7>
2010-07-15 11:38:11 +0000

    Update Assembly info

--- addons/inout.Matroska/My Project/AssemblyInfo.vb
+++ addons/inout.Matroska/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.0.2405")> 
-<Assembly: AssemblyFileVersion("0.0.0.2405")> 
+<Assembly: AssemblyVersion("0.0.0.2408")> 
+<Assembly: AssemblyFileVersion("0.0.0.2408")>
--- addons/inout.XBMC/My Project/AssemblyInfo.vb
+++ addons/inout.XBMC/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.0.2408")> 
-<Assembly: AssemblyFileVersion("0.0.0.2408")> 
+<Assembly: AssemblyVersion("0.0.0.2409")> 
+<Assembly: AssemblyFileVersion("0.0.0.2409")> 
